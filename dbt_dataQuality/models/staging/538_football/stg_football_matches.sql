ebff54ad130d7c79bb98067c4ef10f94a0e67b4b
Yassin Rouani <yassin.rouani@artefact.com>
2023-02-09 16:52:55 +0100

    :add dbt meta testing package

--- dbt_dataQuality/models/staging/538_football/stg_football_matches.sql
+++ dbt_dataQuality/models/staging/538_football/stg_football_matches.sql
@@ -1,3 +1,9 @@
-SELECT *
+WITH matches AS(
 
-FROM {{source('538_football','raw_football_matches')}}
+    SELECT *
+
+    FROM {{source('538_football','raw_football_matches')}}
+    
+)
+
+SELECT *, CONCAT(team1, '_', team2) as id from matches
--- dbt_dataQuality/target/compiled/dbt_dataQuality/models/staging/538_football/stg_football_matches.sql
+++ dbt_dataQuality/target/compiled/dbt_dataQuality/models/staging/538_football/stg_football_matches.sql
@@ -1,3 +1,9 @@
-SELECT *
+WITH matches AS(
 
-FROM `dbt-for-bigquery-377112`.`538_football`.`raw_football_matches`
+    SELECT *
+
+    FROM `dbt-for-bigquery-377112`.`538_football`.`raw_football_matches`
+    
+)
+
+SELECT *, CONCAT(team1, '_', team2) as id from matches
